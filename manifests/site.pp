1261441d8dfac6e9bf3f34043a963732ea265c42
wilsonhui <wilson.hui@igsl-group.com>
2016-11-30 21:52:01 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,9 @@ node default {
   #   class { 'my_class': }
   include role::classroom
 }
+augeas {
+  context => "files/etc/hosts"
+  changes => [
+    "set <127.0.0.1> <testing.puppetlabs.vm>",
+  ],
+}
