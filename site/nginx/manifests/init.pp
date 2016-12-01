d331b8e980f2572f796865a155ecfa81972ef2dc
darrentwt <darrentwt@yahoo.com>
2016-12-01 23:21:19 +0800

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,38 @@
+class nginx {
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { 'docroot':
+    ensure => directory,
+    path   => '/var/www',
+  }
+  
+  file { 'index.html':
+    path   => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { 'nginx.conf':
+    path   => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  
+  file { 'default.conf':
+    path   => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [ File['default.conf'], File['nginx.conf'] ],
+  }
+}
