baf0340d7aa6b729a7015e356fd58e8fe945b6ba
darrentwt <darrentwt@yahoo.com>
2016-12-02 00:41:04 +0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,35 +1,31 @@
 class nginx {
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+  
   package { 'nginx':
     ensure => present,
   }
   
   file { 'docroot':
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
     path   => '/var/www',
   }
   
   file { 'index.html':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
     path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'nginx.conf':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
     path   => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   
   file { 'default.conf':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
     path   => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
   }
@@ -37,5 +33,6 @@ class nginx {
   service { 'nginx':
     ensure => running,
     enable => true,
+    subscribe => [ File['default.conf'], File['nginx.conf'] ],
   }
 }
